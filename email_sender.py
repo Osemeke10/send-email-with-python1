4360233a9786b18a8fd552d6de556975897e7359
Stephen Osemeke <osemekestephen1995@outlook.com>
2021-07-22 11:42:46 +0200

    Update email_sender.py

--- email_sender.py
+++ email_sender.py
@@ -11,6 +11,6 @@ email.set_content('i am a python Master')
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
     smtp.ehlo()
     smtp.starttls()
-    smtp.login('richardgaily8@gmail.com','Osemeke10')
+    smtp.login('richardgaily8@gmail.com','email_login_ password')
     smtp.send_message(email)
     print('all good boss!')
